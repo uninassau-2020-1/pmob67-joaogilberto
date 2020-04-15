b5b5e64523ba140fec526b6d266124c91f3ccbb8
João Gilberto <joaogilberto_ns@yahoo.com.br>
2020-04-15 14:11:23 -0300

    Alterações de layout e botão de busca.

---
+++ src/pages/home/home.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ViacepProvider} from '../../providers/viacep/viacep';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+	private cep;
+	private endereco:any = {};
+	vazio = "";
+
+  constructor(public navCtrl: NavController, private viacep: ViacepProvider) {}
+
+	getEndereco(){
+		this.viacep.callService(this.cep)
+		.subscribe(
+        data =>{
+		this.endereco = data;
+		console.log(data);
+        }
+      );
+	}
+
+
+
+}
+
